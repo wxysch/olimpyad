001ea40e3d48dafe707fcaec31df184ae24e96f9
alinu1 <uwu1343@gmail.com>
2022-12-29 11:25:01 +0300

    ls17.py

--- ls16.py
+++ ls16.py
@@ -44,26 +44,26 @@
 счисления — 357, а ответ на задачу равен 3 × 5 × 7 − (3 + 5 + 7) = 90.
 '''
 
-a,b = map(int,input().split())
-def per(a,b):
-    res = a%b
-    a = a//b
-    mn = 10
-    while a>0:
-        res = res+((a%b)*mn)
-        mn*=10
-        a=a//b
-    return res 
-def otvet(a):
-    s=0
-    p=1
-    while a>0:
-        x=a%10
-        a = a//10
-        s+=x
-        p*=x
-    return p-s
-print(otvet(per(a,b)))
+# a,b = map(int,input().split())
+# def per(a,b):
+#     res = a%b
+#     a = a//b
+#     mn = 10
+#     while a>0:
+#         res = res+((a%b)*mn)
+#         mn*=10
+#         a=a//b
+#     return res 
+# def otvet(a):
+#     s=0
+#     p=1
+#     while a>0:
+#         x=a%10
+#         a = a//10
+#         s+=x
+#         p*=x
+#     return p-s
+# print(otvet(per(a,b)))
 
 '''
 Необходимо вычислить значение 2n.
